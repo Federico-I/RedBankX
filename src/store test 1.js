32f7667e88c9b96bd41b9fd857332fdab23ad1f6
Federico-I <threxos.48@gmail.com>
2024-08-26 23:59:50 +0100

    account-Reducer

--- src/store test 1.js
+++ src/store test 1.js
@@ -1,10 +1,5 @@
 import { combineReducers, createStore } from "redux";
 
-const initialStateAccount = {
-  balance: 0,
-  loan: 0,
-  loanPuropse: "",
-};
 
 const initialStateCustome = {
   fullName: "",
@@ -12,31 +7,6 @@ const initialStateCustome = {
   createdAt: "",
 } 
 
-function accountReducer(state = initialStateAccount, action) {
-  switch(action.type) {
-    case "action/deposit":
-      return{...state, balance: state.balance + action.payload};
-
-    case "action/withdraw":
-      return{...state, balance: state.balance - action.payload};
-
-    case "account/requestLoan":
-      if (state.loan > 0) return state;
-      // Later
-      return { ...state, loan: action.payload, loanPurpose: action.payload.purpose, balance: state.balance + action.payload.amount,
-      };
-
-    case "account/payLoan":
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: "",
-        balance: state.balance - state.loan,
-      }
-    default: 
-      return state;
-  }
-}
 
 /////////////////////////////////////////////////
 //             Reducer - Logic
@@ -80,32 +50,6 @@ const store = createStore(rootReducer);
 
 // const ACCOUNT_DEPOSIT = "account/deposit";
 
-function deposit(amount) {
-  return { 
-    type: "account/deposit", 
-    payload: amount, 
-  }
-};
-
-function withdraw(amount) {
-  return { 
-    type: "account/withdraw", 
-    payload: amount, 
-  };
-};
-
-function requestLoan(amount, purpose) {
-  return {
-    type: "account/requestLoan",
-    payload: { amount, purpose },
-  };
-}
-
-function payLoan() {
-  return {
-    type: "account/payLoan"
-  };
-}
 
 // console.log(store.getState());
 // store.dispatch({ type: "account/payLoan" });
@@ -125,4 +69,4 @@ function updateName(fullName) {
 
 store.dispatch(createCustomer("Threxos Karnos", "8624907252"));
 store.dispatch(deposit(250));
-console.log(store.getState());
+console.log(store.getState())
